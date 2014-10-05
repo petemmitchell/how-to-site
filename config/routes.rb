af910c336a84eb93060a9c4e226d1c30272ffb8e
Pete Mitchell <petemmitchell@gmail.com>
2014-10-04 22:26:14 -0700

    Added models and controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'guides/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'guides#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -13,6 +15,10 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+
+  resources :guides do
+    resources :steps
+  end
 
   # Example resource route with options:
   #   resources :products do
